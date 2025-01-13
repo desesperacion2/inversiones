3c2b52bc2bcb84d0253732e16e3c94c1ba8cd17a
desesperacion2 <emilioestebansuazo@gmail.com>
2025-01-13 00:39:03 -0300

    intento desesperado

---
+++ backend/install_chrome.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "Installing Google Chrome..."
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt-get install -y ./google-chrome-stable_current_amd64.deb
