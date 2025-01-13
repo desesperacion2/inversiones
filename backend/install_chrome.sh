c4f561bb223355feb33c094a872e8277e9915569
desesperacion2 <emilioestebansuazo@gmail.com>
2025-01-13 00:46:34 -0300

    un commit atras

--- backend/install_chrome.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Installing Google Chrome..."
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-apt-get install -y ./google-chrome-stable_current_amd64.deb
