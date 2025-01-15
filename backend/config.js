c5e762ef29710f6845750395c0d95fd704a0a194
desesperacion2 <emilioestebansuazo@gmail.com>
2025-01-14 23:30:01 -0300

    cambie la version qla del puppeteer

--- backend/config.js
+++ backend/config.js
@@ -1,26 +1,20 @@
 import puppeteer from 'puppeteer';
 
-export const PUPPETEER_CONFIG = process.env.PUPPETEER_EXECUTABLE_PATH
-  ? {
-      executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
-      headless: "new",
-      args: [
-        '--no-sandbox',
-        '--disable-setuid-sandbox',
-        '--disable-dev-shm-usage',
-        '--disable-gpu',
-        '--no-first-run',
-        '--no-zygote',
-        '--single-process'
-      ],
-    }
-  : {
-      headless: "new",
-    };
+export const PUPPETEER_CONFIG = {
+  args: [
+    '--no-sandbox',
+    '--disable-setuid-sandbox',
+    '--disable-dev-shm-usage',
+    '--disable-accelerated-2d-canvas',
+    '--no-first-run',
+    '--no-zygote',
+    '--single-process',
+    '--disable-gpu'
+  ],
+  headless: 'new'
+};
 
 export async function createBrowser() {
-  return process.env.PUPPETEER_EXECUTABLE_PATH
-    ? puppeteer.launch(PUPPETEER_CONFIG)
-    : puppeteer.launch();
+  return puppeteer.launch(PUPPETEER_CONFIG);
 }
 
